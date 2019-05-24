658e535ce04a0f5dabbbfe830a050754ae172b1b
Greg Jordan <gregljordan@gmail.com>
2019-05-24 12:49:14 -0400

    Checking over page and functionality

--- js/init.js
+++ js/init.js
@@ -7,6 +7,11 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
+
 var elem = document.querySelector('.collapsible');
 var instance = M.Collapsible.init(elem, {
   accordion: false
